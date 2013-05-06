106419c03dffd03d33c54094ae9c8682039e74f3
Alexey Chuvashov <alexeychu@gmail.com>
2013-05-06 19:39:34 +0400

    remove scaffold trash

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -15,73 +15,51 @@ class StoriesController < ApplicationController
     redirect_to @story, :notice => notice
   end
 
-  # GET /stories
   def index
     @search = Story.search(params[:q])
     @stories = @search.result(:distinct => true)
-
-    respond_to do |format|
-      format.html # index.html.erb
-    end
   end
 
-  # GET /stories/1
   def show
     @story = Story.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-    end
   end
 
-  # GET /stories/new
   def new
     @story = Story.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-    end
   end
 
-  # GET /stories/1/edit
   def edit
     @story = Story.find(params[:id])
   end
 
-  # POST /stories
   def create
     @story = Story.new(params[:story].merge(:user_id => current_user.id))
 
-    respond_to do |format|
-      if @story.save
-        format.html { redirect_to @story, notice: 'Story was successfully created.' }
-      else
-        format.html { render action: 'new' }
-      end
+    if @story.save then
+      redirect_to @story, notice: 'Story was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
-  # PUT /stories/1
   def update
     @story = Story.find(params[:id])
 
-    respond_to do |format|
-      if @story.update_attributes(params[:story])
-        format.html { redirect_to @story, notice: 'Story was successfully updated.' }
-      else
-        format.html { render action: 'edit' }
-      end
+    if @story.update_attributes(params[:story])
+      redirect_to @story, notice: 'Story was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
-  # DELETE /stories/1
   def destroy
     @story = Story.find(params[:id])
     @story.destroy
 
-    respond_to do |format|
-      format.html { redirect_to stories_url }
-    end
+    redirect_to stories_url
+
   end
 
   private
--- app/controllers/story_comments_controller.rb
+++ app/controllers/story_comments_controller.rb
@@ -2,28 +2,22 @@ class StoryCommentsController < ApplicationController
   before_filter :require_login
   before_filter(:only => [:destroy]) { |sc| sc.require_owner StoryComment.find(params[:id]).user}
 
-  # POST /story_comments
   def create
     @story_comment = StoryComment.new(params[:story_comment].merge(:user_id => current_user.id).merge(:story_id => params[:story_id]))
 
-    respond_to do |format|
       if @story_comment.save
-        format.html { redirect_to @story_comment.story, notice: 'Story comment was successfully created.' }
+        redirect_to @story_comment.story, notice: 'Story comment was successfully created.'
       else
-        format.html { redirect_to request.referer }
+        redirect_to request.referer
       end
-    end
   end
 
-  # DELETE /story_comments/1
   def destroy
     @story_comment = StoryComment.find(params[:id])
-    story = @story_comment.story
     @story_comment.destroy
 
-    respond_to do |format|
-      format.html { redirect_to story_path(story) }
-    end
+    redirect_to story_path(@story_comment.story)
+
   end
 
 end
