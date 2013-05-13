98b3bae20cdc64e81b5a6b08f415fd1ca2a2e1fb
Alexey Chuvashov <alexeychu@gmail.com>
2013-05-13 12:23:59 +0400

    improve routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Taskmanager::Application.routes.draw do
 
-  get 'logout' => 'sessions#destroy', :as => 'logout'
-  get 'login' => 'sessions#new', :as => 'login'
-  get 'signup' => 'users#new', :as => 'signup'
   root :to => 'stories#index'
 
   resources :sessions, :only => [:new, :create, :destroy]
 
   resources :stories do
-    member do
-      get 'next_state'
-    end
+    put :next_state, :on => :member
     resources :story_comments, :only => [:create, :destroy]
   end
   resources :users, :only => [:new, :create, :destroy]
