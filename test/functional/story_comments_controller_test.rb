554831fdc20c6f702368e2672bce4af3b3fd483a
Alexey Chuvashov <alexeychu@gmail.com>
2013-05-14 04:35:03 +0400

    add factories and tests

--- test/functional/story_comments_controller_test.rb
+++ test/functional/story_comments_controller_test.rb
@@ -1,49 +1,27 @@
 require 'test_helper'
 
-class StoryCommentsControllerTest < ActionController::TestCase
+class Web::StoryCommentsControllerTest < ActionController::TestCase
+
   setup do
-    @story_comment = story_comments(:one)
-  end
-
-  test 'should get index' do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:story_comments)
-  end
-
-  test 'should get new' do
-    get :new
-    assert_response :success
+    @user = create :user
+    sign_in @user
+    @story = create :story
+    @story_comment = create :story_comment
   end
 
   test 'should create story_comment' do
+    attrs = attributes_for :story_comment
     assert_difference('StoryComment.count') do
-      post :create, story_comment: { text: @story_comment.text }
+      post :create, story_comment: attrs, story_id: @story
     end
-
-    assert_redirected_to story_comment_path(assigns(:story_comment))
-  end
-
-  test 'should show story_comment' do
-    get :show, id: @story_comment
-    assert_response :success
-  end
-
-  test 'should get edit' do
-    get :edit, id: @story_comment
-    assert_response :success
-  end
-
-  test 'should update story_comment' do
-    put :update, id: @story_comment, story_comment: { text: @story_comment.text }
-    assert_redirected_to story_comment_path(assigns(:story_comment))
+    assert_response :redirect
   end
 
   test 'should destroy story_comment' do
+    sign_in @story_comment.user
     assert_difference('StoryComment.count', -1) do
-      delete :destroy, id: @story_comment
+      delete :destroy, id: @story_comment, story_id: @story
     end
-
-    assert_redirected_to story_comments_path
+    assert_response :redirect
   end
 end
