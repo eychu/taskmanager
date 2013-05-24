028dd206d4ffb6413cb17aa6c905b78320577b4e
Alexey Chuvashov <alexeychu@gmail.com>
2013-05-24 15:15:29 +0400

    remove format.html

--- app/controllers/web/users_controller.rb
+++ app/controllers/web/users_controller.rb
@@ -3,10 +3,6 @@ class Web::UsersController < Web::ApplicationController
   # GET /users/new
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-    end
   end
 
   # POST /users
@@ -16,9 +12,9 @@ class Web::UsersController < Web::ApplicationController
     respond_to do |format|
       if @user.save
         sign_in @user
-        format.html { redirect_to root_path, notice: 'User was successfully created.' }
+        redirect_to root_path, notice: 'User was successfully created.'
       else
-        format.html { render action: 'new' }
+        render action: 'new'
       end
     end
   end
