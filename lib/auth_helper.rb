14046b46b92decff2a2ae4902a7c59854cc3ec14
Alexey Chuvashov <alexeychu@gmail.com>
2013-05-29 10:14:00 +0400

    requare login to auth_helper

--- lib/auth_helper.rb
+++ lib/auth_helper.rb
@@ -16,4 +16,8 @@ module AuthHelper
     @current_user ||= User.find_by_id(session[:user_id])
   end
 
+  def require_login
+    redirect_to root_path, notice: t('please_login') unless signed_in?
+  end
+
 end
